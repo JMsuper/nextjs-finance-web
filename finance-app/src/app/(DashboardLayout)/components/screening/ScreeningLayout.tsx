f730b34c59a35b817ef85139bc6a3dee285d008e
JMsuper <tjsqls2067@naver.com>
2024-03-29 22:35:04 +0900

    Remove commented out code and update StepThree and StepFour components

--- finance-app/src/app/(DashboardLayout)/components/screening/ScreeningLayout.tsx
+++ finance-app/src/app/(DashboardLayout)/components/screening/ScreeningLayout.tsx
@@ -10,34 +10,25 @@ import StepZero from './StepZero';
 import DashboardCard from '../shared/DashboardCard';
 import useFetch from '@/app/hooks/useFetch';
 import { StockFinanceInfo } from './StockFinanceInfo';
-import { Rowdies } from 'next/font/google';
 
-const StepThreePart = () => {
-    // Implement the Future Value Estimation Tool here
-    return <div>- 현재 주당순자산가치에서 예상 ROE를 적용해 미래 주당순자산가치를 산정한다.<br></br>
-        - 10년 후 주당순자산가치 = 현재 주당순자산가치 * (1 + 예상 ROE)^10
-        <StepThree></StepThree>
-    </div>;
-};
+// const StepThreePart = () => {
+//     // Implement the Future Value Estimation Tool here
+//     return <div>- 현재 주당순자산가치에서 예상 ROE를 적용해 미래 주당순자산가치를 산정한다.<br></br>
+//         - 10년 후 주당순자산가치 = 현재 주당순자산가치 * (1 + 예상 ROE)^10
+//         <StepThree></StepThree>
+//     </div>;
+// };
 
-const StepFourPart = () => {
-    // Implement the tool for calculating expected returns here
-    return <div>- 현재의 주가를 대입해 기대수익률을 산정한다.<br></br>
-        - 기대수익률 = (10√(‘10년 후 주당순자산가치’ / ‘현재의 주가’)) - 1<br></br>
-        - 산정된 기대수익률이 연 목표수익률을 초과하면 매수한다
-        <StepFour></StepFour>
-    </div>;
-};
+// const StepFourPart = () => {
+//     // Implement the tool for calculating expected returns here
+//     return <div>- 현재의 주가를 대입해 기대수익률을 산정한다.<br></br>
+//         - 기대수익률 = (10√(‘10년 후 주당순자산가치’ / ‘현재의 주가’)) - 1<br></br>
+//         - 산정된 기대수익률이 연 목표수익률을 초과하면 매수한다
+//         <StepFour></StepFour>
+//     </div>;
+// };
 
 const steps = ['0단계 : 대상종목 선정','1단계 : 현재가치 확인', '2단계 : 미래수익률 예측', '3단계 : 미래가치 산정', '4단계 : 기대수익률 산정'];
-
-// TODO: selectedRows 배열이 직접 수정되고 있습니다.
-// 배열을 직접 수정하는 것은 React의 불변성 원칙에 어긋나며, 이로 인해 예상치 못한 버그가 발생할 수 있습니다.
-// 대신, 상태를 업데이트할 때는 새로운 배열을 생성하여 사용하는 것이 좋습니다.
-
-// TODO: getStepContent 함수 내에서 사이드 이펙트가 발생합니다.
-// 컴포넌트의 렌더링 로직 내에서 사이드 이펙트(예: 상태 업데이트)를 발생시키는 것은 좋지 않습니다. 
-// 대신, useEffect 훅을 사용하여 의존성 배열에 selected를 넣어 선택된 항목이 변경될 때만 필터링 로직이 실행되도록 하는 것이 바람직합니다.
 
 const ScreeningLayout = () => {
     const [activeStep, setActiveStep] = useState(0);
@@ -91,9 +82,9 @@ const ScreeningLayout = () => {
             case 2:
                 return <StepTwo rows={selectedRows}/>;
             case 3:
-                return <StepThreePart />;
+                return <StepThree rows={selectedRows} />;
             case 4:
-                return <StepFourPart />;
+                return <StepFour rows={selectedRows} />;
             default:
                 return (<div></div>)
         }
