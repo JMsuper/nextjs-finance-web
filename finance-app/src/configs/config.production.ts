e2a213f5b4f6a092295f3818e949acf874b2365d
JMsuper <tjsqls2067@naver.com>
2024-04-13 23:53:12 +0900

    Update production base URL in config.production.ts

--- finance-app/src/configs/config.production.ts
+++ finance-app/src/configs/config.production.ts
@@ -1,7 +1,6 @@
 import getConfigs from './config.common';
 
-const baseUrl =
-  'http://ec2-3-37-127-31.ap-northeast-2.compute.amazonaws.com:80';
+const baseUrl = 'https://snowball-stock.site';
 const mode = 'production';
 
 const configProduction = getConfigs({ baseUrl, mode });
